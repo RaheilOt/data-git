2dc95e9f11a2c973a22ecc6d9bf3181728752faf
raheil <raheil@mak-bwk-alkhas-b-raheil.local>
2025-09-02 02:42:27 +0300

    Implement my_name_is and my_buddy_is functions with docstrings

--- today.py
+++ today.py
@@ -1,6 +1,12 @@
 '''
 This is the module docstring
 '''
+"""A module computing buddy pair names for the day"""
 
+def my_name_is():
+    """Returns my GitHub nickname"""
+    return "RaheilOt"  # Replace with your own GitHub nickname
 
-pass  # YOUR CODE HERE
+def my_buddy_is():
+    """Returns my buddy's GitHub nickname"""
+    return "KhaledAlthumairiy"  # My buddy's GitHub nickname
